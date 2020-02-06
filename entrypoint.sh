84fbb39208d7f6bba18f39375f88e625912b36c0
Wim Winterberg <wim@we.are.builders>
2020-02-06 14:00:40 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-echo "ARGUMENTS: $1"
-echo "ARGUMENTS: $@"
+if [ -z "$1" ]; then
+    CMD_ARGS=""
+else
+    CMD_ARGS="$@"
+fi
 
-printenv
+eval $(ssh-agent -s)
 
+echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
+chmod 600 /tmp/id_rsa
+ssh-add /tmp/id_rsa
+
+# download deployer from repository
+curl -sS -H 'Authorization: token ${GITHUB_TOKEN}' --location --request GET 'https://raw.githubusercontent.com/'\"${GITHUB_REPOSITORY}\"'/'\"${GITHUB_SHA}\"''\"${INPUT_DEPLOYER-FILE-LOCATION}\"'' > deploy.php
+
+deployer --version
+deployer--file=./deploy.php -v deploy
