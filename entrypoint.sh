3434b21e52e305a7f3a5ea4ffc0f1f8377ffcc62
Wim Winterberg <wim@we.are.builders>
2020-02-06 13:52:04 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-if [ -z "$1" ]; then
-    CMD_ARGS=""
-else
-    CMD_ARGS="$@"
-fi
+echo "ARGUMENTS: $1"
+echo "ARGUMENTS: $@"
 
-eval $(ssh-agent -s)
+printenv
 
-echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
-chmod 600 /tmp/id_rsa
-ssh-add /tmp/id_rsa
-
-deployer --version
-deployer $CMD_ARGS
