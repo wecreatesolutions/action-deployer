fbac5d35291e3b07e645d4ff50c0280e2b53572d
Wim Winterberg <wim@we.are.builders>
2020-02-06 10:23:54 +0100

    initial version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 eval $(ssh-agent -s)
 
-#echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-#echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
-#chmod 600 /tmp/id_rsa
-#ssh-add /tmp/id_rsa
-#
-#deployer --version
-#deployer deploy
+echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
+chmod 600 /tmp/id_rsa
+ssh-add /tmp/id_rsa
+
+deployer --version
+deployer deploy
