89df0ed8d1e9b035615861283c82d88f2cc4b382
Wim Winterberg <wim@we.are.builders>
2020-02-06 12:47:53 +0100

    initial version

--- recipes/bootstrap_github_action.php
+++ recipes/bootstrap_github_action.php
@@ -40,6 +40,7 @@ if (in_array($branch, ['production', 'master'])) {
 // endregion
 
 // set configuration
+writeln(sprintf('Deployment target is <info>%1$s</info>', $stage));
 set('default_stage', $stage);
 set('revision', $revision);
 set('branch', $branch);
