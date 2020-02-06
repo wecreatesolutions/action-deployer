4c89ff87b73d3cc1dad4ee55cb422831cfbc89d4
Wim Winterberg <wim@we.are.builders>
2020-02-06 16:09:55 +0100

    # v0.0.3 * download deploy configuration from repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v0.0.3
+* download deploy configuration from repo
+
 # v0.0.2
 * fixed typo in var
 * adjusted slack message
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-if [ -z "$1" ]; then
-    CMD_ARGS=""
-else
-    CMD_ARGS="$@"
-fi
-
 eval $(ssh-agent -s)
 
 echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
@@ -15,5 +9,22 @@ echo "$SSH_PRIVATE_KEY" | tr -d '\r' > /tmp/id_rsa
 chmod 600 /tmp/id_rsa
 ssh-add /tmp/id_rsa
 
+# region $INPUT_VERBOSELEVEL
+if [ -z "$INPUT_VERBOSELEVEL" ]
+then
+  INPUT_VERBOSELEVEL='v'
+fi
+#endregion
+
+# region $INPUT_DEPLOYERFILELOCATION
+if [ -z "$INPUT_DEPLOYERFILELOCATION" ]
+then
+  INPUT_DEPLOYERFILELOCATION='.deployment/deploy.php'
+fi
+#endregion
+
+# download deployer from repository
+curl -sS -H 'Authorization: token '"${GITHUB_TOKEN}"'' --location --request GET 'https://raw.githubusercontent.com/'"${GITHUB_REPOSITORY}"'/'"${GITHUB_SHA}"'/'"${INPUT_DEPLOYERFILELOCATION}"'' > deploy.php
+
 deployer --version
-deployer $CMD_ARGS
+deployer --file=./deploy.php -$INPUT_VERBOSELEVEL deploy
